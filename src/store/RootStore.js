1b2b40290ae2322887a1d08fab4506deb711989d
BlessedVictim <tequila1000@yandex.ru>
2019-04-06 00:24:22 +0300

    fAck

---
+++ src/store/RootStore.js
@@ -0,0 +1,13 @@
+import MessagesStore from "./MessagesStore";
+import AccountStore from "./AccountStore";
+import WebSocketService from "../services/websocketService";
+
+class RootStore{
+    constructor(){
+        this.webSocketService = new WebSocketService(this);
+        this.accountStore = new AccountStore(this);
+        this.messagesStore = new MessagesStore(this.accountStore);
+    }
+}
+
+export default new RootStore();
