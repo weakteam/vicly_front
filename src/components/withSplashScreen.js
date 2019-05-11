0e24450c84e0ca4d2768cb40efdb056a089b57a3
blessedvictim <tequila1000@yandex.ru>
2019-05-11 04:56:55 +0400

    splash screen part 2 {ANIMATE}

--- src/components/withSplashScreen.js
+++ src/components/withSplashScreen.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import logo from "../images/icon512.png";
+import logo from "../images/icon192.png";
 import {withStyles} from "@material-ui/core";
+import Fade from "@material-ui/core/Fade";
+import Slide from "@material-ui/core/Slide";
+import Zoom from "@material-ui/core/Zoom";
 
 class LoadingMessage extends React.Component {
     render() {
@@ -52,10 +55,28 @@ function withSplashScreen(WrappedComponent) {
 
         render() {
             // while checking user session, show "loading" message
-            if (this.state.loading) return <Loading/>;
+
 
             // otherwise, show the desired route
-            return <WrappedComponent {...this.props} />;
+            return (
+                <>
+                    <Slide direction="up" mountOnEnter unmountOnExit in={this.state.loading} timeout={{
+                        appear: 500,
+                        enter: 300,
+                        exit: 0,
+                    }}>
+                        <Loading/>
+                    </Slide>
+                    <Slide appear={true} direction="down" mountOnEnter unmountOnExit in={!this.state.loading} timeout={{
+                        appear: 500,
+                        enter: 300,
+                        exit: 500,
+                    }}>
+                        <WrappedComponent {...this.props} />
+                    </Slide>
+
+                </>
+            )
         }
     };
 }
